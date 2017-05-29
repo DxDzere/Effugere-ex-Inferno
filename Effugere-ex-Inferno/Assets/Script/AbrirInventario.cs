f46011880d62ff5d3ea703e852500d9b19e17de8
zere-PC\zere <nikorasugema@gmail.com>
2017-05-29 13:18:20 -0300

    inventario

---
+++ Effugere-ex-Inferno/Assets/Script/AbrirInventario.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbrirInventario : MonoBehaviour {
+
+	public GameObject CamaraJugador;
+	public GameObject CamaraInventario;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.I)) {
+			CamaraJugador.SetActive (false);
+			CamaraInventario.SetActive (true);
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			CamaraJugador.SetActive (true);
+			CamaraInventario.SetActive (false);
+		}
+	}
+}
